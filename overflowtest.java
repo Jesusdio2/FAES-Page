7a164cbf04de78efd15c3a90e7f794c4313b748a
Francisco <frankwither991000@gmail.com>
2025-10-31 14:24:20 -0700

    Add OverflowCrash class to test integer overflow
    Demonstrates handling of integer overflow using Math.addExact.

---
+++ overflowtest.java
@@ -0,0 +1,10 @@
+public class OverflowCrash {
+    public static void main(String[] args) {
+        int x = Integer.MAX_VALUE; // 2_147_483_647
+
+        // Esto lanza ArithmeticException si hay desbordamiento
+        int y = Math.addExact(x, 1);
+
+        System.out.println("Resultado: " + y);
+    }
+}
