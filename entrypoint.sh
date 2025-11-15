dc1ec9206ad819a2c67b9a2d2be8219c6c8cb8eb
Francisco <frankwither991000@gmail.com>
2025-11-15 12:25:42 -0700

    Enhance entrypoint.sh with debug echo statements
    Added echo statements for debugging and verification.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "01"
+ls -l /etc/anubis/decaymap.yaml   # Verificación de que el archivo existe
 /usr/local/bin/anubis -bind :3923 -policy-fname /etc/anubis/decaymap.yaml &
 
+echo "Nginx"
 exec nginx -g "daemon off;"
