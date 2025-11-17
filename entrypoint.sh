a54cca7847949d8e2abfeb301793eb7ed77af35b
Francisco <frankwither991000@gmail.com>
2025-11-16 19:31:58 -0700

    Commit Error

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "01"
 ls -l /etc/anubis/decaymap.yaml   # Verificación de que el archivo existe
-/usr/local/bin/anubis -bind :3923 -policy-fname /etc/anubis/decaymap.yaml &
+/usr/local/bin/anubis -bind :3923 -policy-fname /etc/anubis/decaymap.yaml -use-remote-address &
 
 echo "Nginx"
 exec nginx -g "daemon off;"
