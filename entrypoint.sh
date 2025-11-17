8951fbdcc9def91362429e06bbec40fc6de9841c
Francisco <frankwither991000@gmail.com>
2025-11-16 19:43:19 -0700

    Update entrypoint.sh
    Added command to start Nginx in the foreground.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 echo "01"
 ls -l /etc/anubis/decaymap.yaml   # Verificación de que el archivo existe
+
 /usr/local/bin/anubis -bind :3923 -policy-fname /etc/anubis/decaymap.yaml -use-remote-address &
 
 echo "Nginx"
+# Arrancar Nginx en primer plano
 exec nginx -g "daemon off;"
