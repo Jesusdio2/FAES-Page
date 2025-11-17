d5554def28778a7de4de6f03ac300e03ce29e609
Francisco <frankwither991000@gmail.com>
2025-11-16 19:52:56 -0700

    Validate Nginx configuration before startup
    Added validation for Nginx configuration before starting.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 echo "01"
-ls -l /etc/anubis/decaymap.yaml   # Verificación de que el archivo existe
+ls -l /etc/anubis/decaymap.yaml
 
 /usr/local/bin/anubis -bind :3923 -policy-fname /etc/anubis/decaymap.yaml -use-remote-address &
 
 echo "Nginx"
+# Validar configuración antes de arrancar
+nginx -t -c /etc/nginx/nginx.conf || exit 1
+
 # Arrancar Nginx en primer plano
 exec nginx -g "daemon off;"
