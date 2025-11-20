257fa605f2d695198f31be17e9ae5308014fa244
Francisco <frankwither991000@gmail.com>
2025-11-19 20:56:29 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
 
-// Endpoint dinámico
 app.get("/api/ritual", (req, res) => {
   res.json({ message: "Anubis activo en el altar FAES" });
 });
 
-// Servir tu FAES.html si lo subes al contenedor
 app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 9090;
