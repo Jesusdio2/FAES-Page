5b9e973783bad47b0c64e4528cffc4192c7f778c
Francisco <frankwither991000@gmail.com>
2025-12-05 21:55:27 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
 
 const supabaseUrl = "https://neswgqikcsxspwdrxqdj.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5lc3dncWlrY3N4c3B3ZHJ4cWRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzMDUwNTEsImV4cCI6MjA3OTg4MTA1MX0.Fmf9_nFRPxZ9Srg409REF4JNhfdP-gCXpNnfIvS4SW4"
+const supabaseKey = "sb_publishable_nfU_SI8z2NhLMVevbEF0Rw_AsnoSTK4"
 
 // Inicializa Supabase con opciones seguras
 let supabase = null
